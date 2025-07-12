f8159a66430c7e5113e1bcdcda57e56f32770b37
Dustin Griffith <djarekg@gmail.com>
2025-07-11 20:52:01 -0400

    initial project setup

---
+++ app/routes.ts
@@ -0,0 +1,17 @@
+import {
+  type RouteConfig,
+  index,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
+
+export default [
+  index("routes/index.tsx"),
+  ...prefix("users", [
+    index("routes/users/index.tsx"),
+    ...prefix(":id", [
+      index("routes/users/[id]/index.tsx"),
+      route("settings", "routes/users/[id]/settings.tsx"),
+    ]),
+  ]),
+] satisfies RouteConfig;
